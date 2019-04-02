1b0bb079e9876aa0cea3221c861f75d3366bb8ce
darthbeeblebrox <charles.fieseler@gmail.com>
2019-04-01 19:09:29 -0700

    Refactored tests to be in multiple sub-folders for the very different functions; removed tests for old functionality

--- run_tests.m
+++ run_tests.m
@@ -1,6 +1,33 @@
-% Runs all tests in the folder
+function [results, suites] = run_tests(which_tests)
+% Runs some set of tests:
+%   1 - sparse residual analysis functions
+%   2 - CElegansModel object (base is the DMDc algorithm)
+%   3 - TODO (will be sparse encoding analysis)
+if ~exist('which_tests', 'var')
+    which_tests = 1:2;
+end
+
+results = table();
+suites = table();
+
 warning('Turning warnings off because they are useless')
 warning off
-suite = matlab.unittest.TestSuite.fromFolder('./tests/');
-result = run(suite);
+if ismember(1, which_tests)
+    disp('Running tests on Sparse Residual Analysis functions')
+    suite_SRA = matlab.unittest.TestSuite.fromFolder(...
+        './tests/Sparse_residual_analysis_tests');
+    result_SRA = run(suite_SRA);
+    results = my_append(results, {result_SRA});
+    suites = my_append(suites, {suite_SRA});
+end
+if ismember(2, which_tests)
+    disp('Running tests on CElegansModel object and functions')
+    suite_CEM = matlab.unittest.TestSuite.fromFolder(...
+        './tests/CElegansModel_tests');
+    result_CEM = run(suite_CEM);
+    results = my_append(results, {result_CEM});
+    suites = my_append(suites, {suite_CEM});
+end
+
 warning on
+end
---
+++ tests/Sparse_residual_analysis_tests/SparseResidualAnalysisTest_one_model.m
@@ -0,0 +1,81 @@
+classdef SparseResidualAnalysisTest_one_model < matlab.unittest.TestCase
+    % CElegansModelTest tests inputs and basic processing properties for
+    % Kato-type structs
+    
+    properties
+        % Model settings
+        filename
+        settings
+        model
+        
+        % Residual analysis settings
+        num_iter
+        rank
+        
+        % Residual analysis output
+        all_U
+        all_acf
+        all_nnz
+    end
+    
+    methods(TestClassSetup)
+        function setFilename(testCase)
+            tmp = get_Zimmer_filenames();
+            testCase.filename = tmp{1};
+            % Calculate the model
+            testCase.settings = define_ideal_settings();
+            testCase.model = ...
+                CElegansModel(testCase.filename, testCase.settings);
+            % Run the sparse residual analysis function
+            testCase.num_iter = 10;
+            testCase.rank = 5;
+            [testCase.all_U, testCase.all_acf, testCase.all_nnz] = ...
+                sparse_residual_analysis_max_acf(...
+                testCase.model, testCase.num_iter, testCase.rank);
+        end
+    end
+    
+    methods (Test)
+        function test_all_U(testCase)
+            % The learned control signals
+            U = testCase.all_U;
+            U1 = U{1};
+            
+            testCase.verifyEqual(...
+                length(U), length(testCase.rank));
+            
+            % Only one rank tested here, not multiple
+            testCase.verifyEqual(...
+                size(U1,1), testCase.rank);
+        end
+        
+        function test_all_acf(testCase)
+            % Properties of the autocorrelation
+            a = testCase.all_acf;
+            
+            testCase.verifyEqual(...
+                size(a{1}, 1), testCase.rank);
+            testCase.verifyEqual(...
+                size(a{1}, 2), testCase.num_iter);
+            
+            testCase.verifyTrue(...
+                isempty(find(a{1}<-1, 1)));
+            testCase.verifyTrue(...
+                isempty(find(a{1}>1, 1)));
+        end
+        
+        function test_all_nnz(testCase)
+            % Properties of the number of nonzeros in the control signals
+            n = testCase.all_nnz{1};
+            
+            % Bounds
+            testCase.verifyTrue(isempty(find(n<0, 1)))
+            testCase.verifyTrue(...
+                isempty(find(n>numel(testCase.all_U{1}),1)));
+            % Decreasing, though it might stall
+            testCase.verifyTrue(...
+                isempty(find(diff(n)>0,1)));
+        end
+    end
+    
+end 
