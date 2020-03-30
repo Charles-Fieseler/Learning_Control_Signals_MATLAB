51e864c480777b29d962b815d13f93aebc60bf38
Charles-Fieseler <konda@uw.edu>
2020-03-30 16:12:16 -0700

    Bring in changes from Zimmer collaboration

--- util/dmdc_cross_val.m
+++ util/dmdc_cross_val.m
@@ -1,49 +1,43 @@
 function [err, all_err] = dmdc_cross_val(X, U, num_folds, err_steps, ...
-    cross_val_mode, is_inclusive, start_fold)
+    cross_val_mode, is_inclusive)
 % Does 'num_folds' cross validation with a DMDc framework
 %   Can use error steps that are greater than 1
-%
-% Input:
-%   X - data (columns are time slices)
-%   U - control signals
-%   num_folds - number of k-folds
-%   err_steps (1) - number of steps in the future to calculate the error
-%   cross_val_mode ('chaining') - by default does a "chained" or "rolling"
-%       cross-validation, which uses as test data the block exactly after
-%       the training block(s). This is recommended for time-series data
-%   is_inclusive (true) - Includes the error for multiple time steps, if 
-%       err_steps>1... otherwise does nothing
-%   start_fold (1) - the fold index to start on. With chaining, the first
-%       fold or two can be an extremely small amount of data and therefore
-%       much much worse
 if ~exist('err_steps', 'var')
     err_steps = 1;
 end
 if ~exist('cross_val_mode', 'var') || isempty(cross_val_mode)
     cross_val_mode = 'chaining';
 end
-if ~exist('is_inclusive', 'var') || isempty(is_inclusive)
+if ~exist('is_inclusive', 'var')
     is_inclusive = true;
+    err_steps = 1:err_steps; % Save each step; average at the end
 end
-if ~exist('start_fold', 'var')
-    start_fold = 1;
-end
+
 
 m = size(X, 2);
 window_starts = round(linspace(1, m, num_folds+1));
 if strcmp(cross_val_mode, 'chaining')
-    window_starts(1:start_fold) = [];
+    window_starts(1) = [];
+    num_folds = num_folds - 1;
 end
-all_err = zeros(1, length(window_starts)-1);
-% Main loop
+
+% Return a set of error evaluations
+if length(err_steps) > 1
+    err_steps_to_save = err_steps;
+    err_steps = err_steps(end); % This should be a scalar
+    all_err = zeros(length(err_steps_to_save), num_folds);
+else
+    all_err = zeros(1, num_folds);
+    err_steps_to_save = err_steps;
+end
 all_ind = 1:(m-err_steps);
 n = size(X, 1);
-for iFold = 1:length(window_starts)-1
+for i = 1:num_folds
     % Get test and training indices
-    test_ind = window_starts(iFold):(window_starts(iFold+1)-err_steps);
+    test_ind = window_starts(i):(window_starts(i+1)-err_steps);
     
     if strcmp(cross_val_mode, 'chaining')
-        train_ind = 1:test_ind(iFold) - err_steps;
+        train_ind = 1:test_ind(i) - err_steps;
     else
         train_ind = all_ind;
         train_ind(test_ind) = [];
@@ -54,7 +48,7 @@ for iFold = 1:length(window_starts)-1
     X2 = X(:, train_ind + 1);
     U1 = U(:, train_ind);
     X1_t = X(:, test_ind);
-    X_end_t = X(:, test_ind+err_steps);
+%     X_end_t = X(:, test_ind+err_steps);
     U1_t = U(:, test_ind(1):(test_ind(end)+err_steps-1));
     
     % Build DMDc model
@@ -67,14 +61,22 @@ for iFold = 1:length(window_starts)-1
     X_hat = X1_t;
     for i2 = 1:err_steps
         X_hat = A*X_hat + B*U1_t(:, i2:(end-(err_steps-i2)));
-        if is_inclusive
-            all_err(iFold) = all_err(iFold) + ...
-                norm(X(:, test_ind+i2) - X_hat, 'fro') / err_steps;
+        if ismember(i2,err_steps_to_save)
+            save_ind = ismember(err_steps_to_save, i2);
+            all_err(save_ind, i) = norm(X(:, test_ind+i2) - X_hat, 'fro');
         end
+%         if is_inclusive
+%             all_err(i) = all_err(i) + ...
+%                 norm(X(:, test_ind+i2) - X_hat, 'fro') / err_steps;
+%         end
     end
-    if ~is_inclusive
-        all_err(iFold) = norm(X_end_t - X_hat, 'fro');
-    end
+%     if ~is_inclusive
+%         all_err(i) = norm(X_end_t - X_hat, 'fro');
+%     end
+end
+if is_inclusive
+    % Average over saved steps
+    all_err = mean(all_err,1);
 end
 
 err = mean(all_err);
